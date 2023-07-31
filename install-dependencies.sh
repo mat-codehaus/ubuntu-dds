10d57120951a84926cfcf3c7351567f51ffa542e
Mathew Pink <mat@codehaus.co>
2023-08-01 07:27:39 +0930

    Update depedencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,4 +6,5 @@
 
 # Install dependencies.
 apt-get update -y
-apt-get install -y build-essential clang cmake libasio-dev libboost-thread-dev libboost-system-dev libboost-coroutine-dev libboost-program-options-dev libssl-dev default-jre wget
+apt-get install -y build-essential clang cmake libasio-dev libboost-thread-dev libboost-system-dev libboost-coroutine-dev libboost-program-options-dev libssl-dev default-jre wget \
+  qtbase5-dev qtpositioning5-dev libqwt-qt5-dev libqt5xmlpatterns5-dev qtwebengine5-dev libxml2-dev libvncserver-dev uuid-dev libgps-dev libgeographic-dev libgeographic19
